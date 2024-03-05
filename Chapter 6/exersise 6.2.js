a073e7781a3ecb0609566acd7f93052e19faafcf
Happy Mahlangu <153817052+HappyDevs1@users.noreply.github.com>
2024-03-06 00:54:22 +0200

    Uploading chapter 6

---
+++ Chapter 6/exersise 6.2.js
@@ -0,0 +1,13 @@
+let descriptiveWords = [
+  "beautiful",
+  "tall",
+  "short",
+  "calm"
+];
+
+function getUserName() {
+  var name = prompt("What is your name buddy?");
+  let randomWord = descriptiveWords[Math.floor(Math.random() * descriptiveWords.length)];
+  console.log(randomWord + " " + name);
+};
+getUserName();
