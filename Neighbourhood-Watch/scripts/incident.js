1edb343abf3d9f7aa71d84c8e357f7035742d890
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2024-03-27 23:27:16 +0200

    adding neighbourhood watch app

--- Neighbourhood-Watch/scripts/incident.js
+++ Neighbourhood-Watch/scripts/incident.js
@@ -5,5 +5,70 @@ function openNav() {
 
 function closeNav() {
   document.getElementById("mySidebar").style.width = "0";
-  document.getElementById("main").style.marginLeft= "0";
+  document.getElementById("main").style.marginLeft = "0";
 }
+
+window.onload = function reportIncident() {
+  // Retrieve data from local storage
+  let incidentType = localStorage.getItem('incidentType');
+  let description = localStorage.getItem('description');
+  let location = localStorage.getItem('location');
+  let date = localStorage.getItem('date');
+  const reporter = localStorage.getItem('reporter');
+
+  if (incidentType && description && location && date && reporter) {
+    const incidentContainer = document.getElementById("incident-container");
+    incidentContainer.innerHTML = `
+    <div class="incident-container">
+        <div class="incident-details">
+          <div class="incident-by-text">Reported by</div>
+          <div class="reporter-username">${reporter}</div>
+          <div class="incident-time">${date}</div>
+        </div>
+        <div>Incident type: ${incidentType}</div>
+        <div class="location-text">Location: ${location}</div>
+        <div class="incident-description">Description: ${description}</div>
+        <div class="incident-image"></div>
+      </div>
+      <button id="delete-incident-btn" class="delete-incident-btn">Delete</button>
+      <hr>`
+      const deleteIncidentBtn = document.getElementById("delete-incident-btn");
+      deleteIncidentBtn.style.backgroundColor = "red";
+      deleteIncidentBtn.style.border = "none";
+      deleteIncidentBtn.style.color = "white";
+
+      deleteIncidentBtn.addEventListener("click", removeIncident);
+      function removeIncident() {
+        incidentContainer.innerHTML = '';
+        localStorage.clear('incidentType');
+        localStorage.clear('description');
+        localStorage.clear('location');
+        localStorage.clear('date');
+        localStorage.clear('reporter');
+      }
+  }
+};
+
+document.getElementById("incident-btn").addEventListener("click", function() {
+  // Store data in local storage
+  let incidentType = document.getElementById("incident-type-input").value;
+  let description = document.getElementById("description-input").value;
+  let location = document.getElementById("location-input").value;
+  let date = document.getElementById("date-input").value;
+  const reporter = document.getElementById("username-display").innerHTML;
+
+  localStorage.setItem('incidentType', incidentType);
+  localStorage.setItem('description', description);
+  localStorage.setItem('location', location);
+  localStorage.setItem('date', date);
+  localStorage.setItem('reporter', reporter);
+
+  const confirmationMessage = document.getElementById("confirmation-message");
+  confirmationMessage.innerHTML = `Your incident has been successfully reported`
+  confirmationMessage.style.color = "green";
+  confirmationMessage.style.marginTop = "1vw"
+});
+
+document.getElementById("incident-btn").addEventListener("click", () => {
+  reportIncident()
+});
--- Neighbourhood-Watch/scripts/resources.js
+++ Neighbourhood-Watch/scripts/resources.js
@@ -8,9 +8,86 @@ function closeNav() {
   document.getElementById("main").style.marginLeft= "0";
 }
 
-const modal = document.getElementById("myModal");
-const emergencyContact = document.getElementById("emergency-contact");
+//emergecy contact Modal
+const emergencyContactModal = document.getElementById("emergencyContactModal");
+const emergencyContactLink = document.getElementById("emergency-contact");
+emergencyContactLink.addEventListener("click", () => {
+  emergencyContactModal.style.display = "block";
+});
+const closeEmergencyModal = document.querySelector(".close");
+closeEmergencyModal.addEventListener("click", () => {
+    emergencyContactModal.style.display = "none";
+});
 
-emergencyContact.addEventListener("click", () => {
-  modal.style.display = "block";
+//local police station modal
+const policeStationModal = document.getElementById("policeStationModal");
+const policeStationLink = document.getElementById("local-police");
+policeStationLink.addEventListener("click", () => {
+  policeStationModal.style.display = "block";
+});
+const closePoliceModal = document.getElementById("close-police-station-modal");
+closePoliceModal.addEventListener("click", () => {
+  policeStationModal.style.display = "none";
+});
+
+//neighbourhood watch modal
+const neigbourhoodWatchModal = document.getElementById("neighbourhoodWatchModal");
+const neigbourhoodWatchLink = document.getElementById("neighbourhood-watch");
+neigbourhoodWatchLink.addEventListener("click", () => {
+  neigbourhoodWatchModal.style.display = "block";
+});
+const closeNeigbourhoodWatch = document.getElementById("close-neigbourhood-watch");
+closeNeigbourhoodWatch.addEventListener("click", () => {
+  neigbourhoodWatchModal.style.display = "none";
+});
+
+//crime prevention tips
+const crimePreventionModal = document.getElementById("crimePreventionModal");
+const crimePreventionLink = document.getElementById("crime-prevention");
+crimePreventionLink.addEventListener("click", () => {
+  crimePreventionModal.style.display = "block";
+});
+const closeCrimePrevention = document.getElementById("close-crime-prevention");
+closeCrimePrevention.addEventListener("click", () => {
+  crimePreventionModal.style.display = "none";
+});
+
+//home security checklist
+const homeSecurityModal = document.getElementById("securityChecklistModal");
+const homeSecurityLink = document.getElementById("home-security-checklist");
+homeSecurityLink.addEventListener("click", () => {
+  homeSecurityModal.style.display = "block";
+});
+const closeSecurityModal = document.getElementById("close-security-modal");
+closeSecurityModal.addEventListener("click", () => {
+  homeSecurityModal.style.display = "none";
 })
+
+//Educatinal material code
+function redirect(url) {
+  window.location.href = url;
+};
+const personalSafetyLink = document.getElementById("personal-safety");
+personalSafetyLink.addEventListener("click", () => {
+  redirect("https://www.thehomesecuritysuperstore.com/blogs/the-home-security-superstore-blog/home-security-101-the-basics")
+});
+
+const propertyMarkingLink = document.getElementById("property-marking");
+propertyMarkingLink.addEventListener("click", () => {
+  redirect("https://www.barrybros.com/2020/12/the-ultimate-guide-to-property-marking/");
+});
+
+const watchSignsLink = document.getElementById("neighbourhood-watch-signs");
+const watchSignModal = document.getElementById("neighborhoodWatchModal");
+watchSignsLink.addEventListener("click", () => {
+  watchSignModal.style.display = "block";
+});
+const closeModal = document.getElementById("close-modal");
+closeModal.addEventListener("click", () => {
+  watchSignModal.style.display = "none";
+});
+
+const reportingLink = document.getElementById("incident-reporting");
+reportingLink.addEventListener("click", () => {
+  redirect("../html/incident.html")
+});
